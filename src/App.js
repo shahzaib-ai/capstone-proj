64d8335f8cf19a2418c9e06f8a33e0101a1af7ad
shahzaib-ai <shahzaib1234@outlook.com>
2023-08-03 18:05:46 +0500

    made header and hero section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Highlights from "./components/Highlights";
 import Testimonials from "./components/Testimonials";
 import About from "./components/About";
 import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,25 @@
+import "./Hero.css";
+import "./Button.css";
+import heroImage from "../assets/restauranfood.jpg";
+
 function Hero() {
   return (
-    <div className="hero">
-      <h1>Hero</h1>
-    </div>
+    <section className="hero">
+      <div className="hero__container">
+        <div className="hero__text">
+          <h1>Little Lemon</h1>
+          <h2>Chicago</h2>
+          <p>
+            We are a family owned Mediterranean restaurant, focused on
+            traditional recipes served with a modern twist.
+          </p>
+          <a className="btn" href="#reserve-table">
+            Reserve a Table
+          </a>
+        </div>
+        <img className="hero__image" src={heroImage} alt="hero" />
+      </div>
+    </section>
   );
 }
 
