88a00e7eb794c4f5f17fbdccea9b1ca5b78f180a
shahzaib-ai <shahzaib1234@outlook.com>
2023-08-06 20:32:48 +0500

    made about section and fixed some layout issues

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,33 @@
+import "../styles/About.css";
+
+import aboutImage from "../assets/marioandadrianb.jpg";
+import aboutImage2 from "../assets/restaurantchefb.jpg";
+
 function About() {
   return (
-    <div className="About">
-      <h1>About</h1>
-    </div>
+    <section className="about">
+      <div className="about__container">
+        <h1>Little Lemon</h1>
+        <h2>Chicago</h2>
+        <p>
+          Little lemon is a charming neighborhood bistro that serves simple food
+          and classic cocktails in a lively but casual environment. The
+          restaurant features a locally-sourced menu with daily specials.
+        </p>
+      </div>
+      <div className="about__images">
+        <img
+          className="about__images--img"
+          src={aboutImage}
+          alt="Two chefs smiling"
+        />
+        <img
+          className="about__images--img2"
+          src={aboutImage2}
+          alt="A chef sprinkling ingredients on a dish"
+        />
+      </div>
+    </section>
   );
 }
 
